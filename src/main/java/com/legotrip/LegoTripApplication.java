28172edb6bc6a2ac8f02fb1eacec3cc022154b9d
Sihyun20 <gong6697@naver.com>
2023-06-07 23:34:01 +0900

    Initial commit

---
+++ src/main/java/com/legotrip/LegoTripApplication.java
@@ -0,0 +1,13 @@
+package com.legotrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legotrip/LegoTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legotrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
